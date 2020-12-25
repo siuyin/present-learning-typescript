7b62a6ad6777fe30c6f2fc5cfcfae6aca2005f1c
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-12-26 01:35:12 +0800

    Added jest tests.

---
+++ tests/arith_test.ts
@@ -0,0 +1,9 @@
+import {sum,ThreeNums} from "../arith"
+test('sum',()=>{
+        expect(sum(2,3)).toBe(5)
+})
+
+test('ThreeNums',()=>{
+        let tn=new ThreeNums(2,3,4)
+        expect(tn.sum()).toBe(8)
+})
