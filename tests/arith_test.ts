f209e729e4ac5a4285932179a3dcf306754de9ad
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-12-26 01:52:20 +0800

    Updated arith tests.

--- tests/arith_test.ts
+++ tests/arith_test.ts
@@ -1,4 +1,5 @@
-import {sum,ThreeNums} from "../arith"
+import {sum,ThreeNums,AlsoASumer} from "../arith"
+
 test('sum',()=>{
         expect(sum(2,3)).toBe(5)
 })
@@ -6,4 +7,11 @@ test('sum',()=>{
 test('ThreeNums',()=>{
         let tn=new ThreeNums(2,3,4)
         expect(tn.sum()).toBe(9)
+        expect(tn.sub()).toBe(2-3-4)
+})
+
+test('AlsoASumer',()=>{
+        let aas=new AlsoASumer()
+        expect(aas.sum()).toBe(1)
+        expect(aas.sub()).toBe(-1)
 })
