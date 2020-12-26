2ff6f4900a8bf094479955ab73bcc8ed24abc40e
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-12-26 16:53:39 +0800

    Updated express server.

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
 // server.ts implements a web server capable of
 // servicing multiple concurrent requests.
 
-import express = require('express')
-const app: express.Application = express() // equivalent to function app() { return express() }
+import svr = require('express') // 1 // HL
+const app: svr.Application = svr() // equivalent to function app() { return express() }
 
 app.get("/", function(r,rw) { //r: request, rw: response writer
         rw.send("Hello World!")
